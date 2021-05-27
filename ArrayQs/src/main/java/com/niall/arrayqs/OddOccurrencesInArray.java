1e6f142324f215ffc0fb85bedd2e85dc88948a7f
Niall Campbell <campbellniall@gmail.com>
2021-05-27 16:41:55 +0100

    Added OddOccurrencesInArray code

---
+++ ArrayQs/src/main/java/com/niall/arrayqs/OddOccurrencesInArray.java
@@ -0,0 +1,26 @@
+package com.niall.arrayqs;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author niallcampbell
+ */
+public class OddOccurrencesInArray {
+    
+    public static int getOddOccurance(int [] array) {
+    
+        if(array.length == 0 || (array.length % 2) == 0 ) return -1;
+        
+        Arrays.sort(array);
+        
+        for(int i = 0; i < array.length - 1; i+=2) {
+        
+            if(array[i] != array[i+1]) return array[i];
+            
+        }
+        
+        return array[array.length - 1];
+    }
+    
+}
---
+++ ArrayQs/src/test/java/OddOccurancesTest.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import static com.niall.arrayqs.OddOccurrencesInArray.*;
+
+/**
+ *
+ * @author niallcampbell
+ */
+public class OddOccurancesTest {
+    
+    public OddOccurancesTest() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+    
+    @Test
+    public void given_Array_ReturnOddOccurance() {
+    
+        int [] array = {9,3,9,3,9,7,9};
+        
+        assertEquals(getOddOccurance(array), 7);
+    
+    }
+    
+    @Test
+    public void given_EvenArray_ReturnMinusOne() {
+    
+        int [] array = {9,3,9,3,9,7,9,7};
+        
+        assertEquals(getOddOccurance(array), -1);
+    
+    }
+    
+    @Test
+    public void given_EmptyArray_ReturnMinusOne() {
+    
+        int [] array = {};
+        
+        assertEquals(getOddOccurance(array), -1);
+    
+    }
+
+    
+}
