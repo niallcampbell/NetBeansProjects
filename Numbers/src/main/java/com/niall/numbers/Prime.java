3a754a22cc683a89c497cbc1023cd9a16cbff385
Niall Campbell <campbellniall@gmail.com>
2021-06-26 15:00:15 +0100

    Updated prime numbers

--- Numbers/src/main/java/com/niall/numbers/Prime.java
+++ Numbers/src/main/java/com/niall/numbers/Prime.java
@@ -9,8 +9,6 @@ public class Prime {
     
     public static boolean isPrimeNumber(int value) {
         
-        if(value == 1) return true;
-        
         for(int i = 2; i <= value - 1; i++) {
             if(value % i == 0) return false;
         }
